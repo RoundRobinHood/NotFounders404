40db122e6687eaf78ec68a22c1e4a0a9300d2aab
roundrobinhood <145403614+RoundRobinHood@users.noreply.github.com>
2025-09-27 13:23:12 +0200

    test

--- NotFounders404.java
+++ NotFounders404.java
@@ -1,12 +1,16 @@
 import dev.robocode.tankroyale.botapi.*;
 import dev.robocode.tankroyale.botapi.events.*;
 import java.util.*;
+import java.lang.Math;
 
 public class BotFind {
-  public int x, y;
+  public double x, y;
 
   public int botId;
   public double moveDirection;
+  public double moveSpeed;
+  public double distance;
+  public int turnNumber;
 }
 
 // ------------------------------------------------------------------
@@ -14,31 +18,85 @@ public class BotFind {
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
 public class NotFounders404 extends Bot {
+  public BotFind bot = null;
 
   // The main method starts our bot
   public static void main(String[] args) {
     new NotFounders404().start();
   }
 
+  public double TargetBearing(BotFind find) {
+    var bulletSpeed = 17; // assume fire(1)
+    var botSpeed = find.moveSpeed;
+    var turnNumber = getTurnNumber();
+
+    var dirTo = directionTo(find.x, find.y);
+    
+    var botX = Math.cos(dirTo);
+    var botY = Math.sin(dirTo);
+
+    var rotX = -botY;
+    var rotY = botX;
+
+    var movX = Math.cos(find.moveDirection);
+    var movY = Math.sin(find.moveDirection);
+
+    var desiredBearing = Math.atan2(bulletSpeed, botSpeed);
+
+    // Dot product check
+    if(rotX * movX + rotY * movY > 0) {
+      desiredBearing = -desiredBearing;
+    }
+
+    var bearing = calcGunBearing(directionTo(find.x, find.y));
+
+    return desiredBearing - bearing;
+  }
+
   // Called when a new round is started -> initialize and do some movement
   @Override
   public void run() {
-    setGunTurnRate(20);
+    setRadarTurnRate(45);
     // Repeat while the bot is running
     while (isRunning()) {
       // Tell the game that when we take move, we'll also want to turn right... a lot
       setTurnRight(10_000);
       // Limit our speed to 5
-      setMaxSpeed(5);
-      // Start moving (and turning)
-      forward(10_000);
+      setTargetSpeed(5);
+
+      if(bot != null) {
+        var bearing = -TargetBearing(bot);
+        System.out.println(bearing);
+        if(Math.abs(bearing) > 20) {
+          setGunTurnRate(Math.signum(bearing) * 20);
+        } else {
+          setGunTurnRate(bearing);
+        }
+        if(Math.abs(bearing) < 5) {
+          fire(1);
+          continue;
+        }
+      }
+
+      go();
     }
   }
 
-  // We saw another bot -> fire!
   @Override
   public void onScannedBot(ScannedBotEvent e) {
-    fire(3);
+    var botFind = new BotFind();
+
+    botFind.botId = e.getScannedBotId();
+    botFind.x = e.getX();
+    botFind.y = e.getY();
+    botFind.moveDirection = e.getDirection();
+    botFind.moveSpeed = e.getSpeed();
+    
+    botFind.distance = distanceTo(botFind.x, botFind.y);
+
+    if(bot == null || bot.botId == botFind.botId || bot.distance > botFind.distance) {
+      bot = botFind;
+    }
   }
 
 
